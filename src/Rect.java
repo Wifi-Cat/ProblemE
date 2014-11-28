022734da36dff2affc4c3bf5fa98d9853a9e7de6
Vance Miller <vbmiller@cs.unc.edu>
2014-11-28 14:51:22 -0500

    code runs now

--- src/Rect.java
+++ src/Rect.java
@@ -1,33 +1,60 @@
-
 public class Rect {
 
-static int x0,y0,x1,y1;
-int area;
-public Rect(int x0,int y0, int x1, int y1){
-	this.x0=x0;
-	this.y0=y0;
-	this.x1=x1;
-	this.y1=y1;
-	normalize_pts();
-	this.area=computeArea();
-}
-public static void normalize_pts(){
-	int dx=x1-x0;
-	int dy=y1-y0;
-	if(dx<0){
-		int temp=x0;
-		x0=x1;
-		x1=temp;
-	}
-	if(dy<0){
-		int temp=y0;
-		y0=y1;
-		y1=temp;
-	}
-}
-public static int computeArea(){
-	int dx=x1-x0;
-	int dy=y1-y0;
-	return dx*dy;
-}
+    private int x0, y0, x1, y1;
+
+    public Rect(int x0, int y0, int x1, int y1) {
+        this.x0 = x0;
+        this.y0 = y0;
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public long getArea() {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        return dx * dy;
+    }
+
+    public static Rect normalize(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        if (dx < 0) {
+            int temp = x0;
+            x0 = x1;
+            x1 = temp;
+        }
+        if (dy < 0) {
+            int temp = y0;
+            y0 = y1;
+            y1 = temp;
+        }
+        return new Rect(x0, y0, x1, y1);
+    }
+
+    public static Rect getIntersection(Rect r0, Rect r1) {
+        int xlow, xhigh, ylow, yhigh;
+        // get x coordinates
+        if (r1.x0 > r0.x0) {
+            xlow = r1.x0;
+        } else {
+            xlow = r0.x0;
+        }
+        if (r1.x1 > r0.x1) {
+            xhigh = r0.x1;
+        } else {
+            xhigh = r1.x1;
+        }
+        // get y coordinates
+        if (r1.y0 > r0.y0) {
+            ylow = r1.y0;
+        } else {
+            ylow = r0.y0;
+        }
+        if (r1.y1 > r0.y1) {
+            yhigh = r0.y1;
+        } else {
+            yhigh = r1.y1;
+        }
+        return new Rect(xlow, ylow, xhigh, yhigh);
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,26 +1,8 @@
-
-public class main {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static Rect intersection(Rect R0, Rect R1){
-		int xlow,xhigh,ylow,yhigh;
-		if(R1.x0>R0.x0) xlow=R1.x0;
-		else xlow=R0.x0;
-		if(R1.x1>R0.x1) xhigh=R0.x1;
-		else xhigh=R1.x1;
-		if(R1.y0>R0.y0) ylow=R1.y0;
-		else ylow=R0.y0;
-		if(R1.y1>R0.y1) yhigh=R0.y1;
-		else yhigh=R1.y1;
-		
-		//TODO: did this check intersection?? we covered all cases and made
-		// a new rectangle....check w team.
-	}
-
+/**
+ * Created by Vance Miller on 11/28/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Rect(0,0,1,1));
+    }
 }
